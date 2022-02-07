d8027a69d03981598f4fc10b26ac0b3afb620451
Çağatay İnyurt <cagatayinyurt@outlook.com>
2022-02-07 20:35:19 +0300

    First commit with <3

--- app/src/main/java/com/cagatayinyurt/kotlin_intent/MainActivity.kt
+++ app/src/main/java/com/cagatayinyurt/kotlin_intent/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.cagatayinyurt.kotlin_intent
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.cagatayinyurt.kotlin_intent.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.btnLogin.setOnClickListener(View.OnClickListener {
+            val intent = Intent(this@MainActivity, SecondActivity::class.java)
+            startActivity(intent)
+        })
     }
 }
--- app/src/main/java/com/cagatayinyurt/kotlin_intent/SecondActivity.kt
+++ app/src/main/java/com/cagatayinyurt/kotlin_intent/SecondActivity.kt
@@ -1,11 +1,24 @@
 package com.cagatayinyurt.kotlin_intent
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.cagatayinyurt.kotlin_intent.databinding.ActivitySecondBinding
 
 class SecondActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySecondBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_second)
+        binding = ActivitySecondBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.btnBackPage.setOnClickListener(View.OnClickListener {
+            val intent = Intent(this@SecondActivity, MainActivity::class.java)
+            startActivity(intent)
+        })
     }
 }
